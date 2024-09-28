919f35d867a748835ef3684dcd8fb33de5e6d5cb
Hyojun Yoon <hyoyoon@cx2r7s7.42seoul.kr>
2024-09-28 17:12:50 +0900

    builtin_error 수정

--- src/builtin/builtin_error.c
+++ src/builtin/builtin_error.c
@@ -6,16 +6,17 @@
 /*   By: hyoyoon <hyoyoon@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/23 18:28:40 by hyoyoon           #+#    #+#             */
-/*   Updated: 2024/09/23 18:32:25 by hyoyoon          ###   ########.fr       */
+/*   Updated: 2024/09/28 16:21:13 by hyoyoon          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
 //TEST 필요
-int	builtin_error(char *str, char *token)
+void	builtin_error(char *str, char *token)
 {
 	write(STDERR_FILENO, str, ft_strlen(str));
-	write(STDERR_FILENO, token, ft_strlen(token));
+	if (token != NULL)
+		write(STDERR_FILENO, token, ft_strlen(token));
 	write(STDERR_FILENO, "\n", 1);
 }
