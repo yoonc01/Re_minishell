ea70b86d21f74f9bbb4ef4aff72dec51145e4468
Youngho Cho <ycho2@cx2r8s3.42seoul.kr>
2024-10-03 11:51:27 +0900

    fix cd to update pwd oldpwd

--- src/builtin/cd.c
+++ src/builtin/cd.c
@@ -3,39 +3,89 @@
 /*                                                        :::      ::::::::   */
 /*   cd.c                                               :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: hyoyoon <hyoyoon@student.42.fr>            +#+  +:+       +#+        */
+/*   By: ycho2 <ycho2@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/23 15:05:39 by hyoyoon           #+#    #+#             */
-/*   Updated: 2024/09/29 13:01:39 by hyoyoon          ###   ########.fr       */
+/*   Updated: 2024/10/03 11:50:23 by ycho2            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
+static void	ft_update_pwd_oldpwd(char *oldpwd,
+				char *pwd, t_blackhole *blackhole);
+static int	cd_dir(char *dir);
+static int	cd_home(t_blackhole *blackhole);
+static int	cd_fail(char *oldpwd);
+
 int	ft_cd(t_blackhole *blackhole)
 {
-	int				ret;
-	char			*home_value;
 	t_inner_block	*current_node;
+	char			*oldpwd;
+	char			*pwd;
 
 	current_node = blackhole->parsed_input->cmd_list->head->next;
+	oldpwd = getcwd(NULL, 0);
 	if (current_node != NULL)
 	{
-		ret = chdir(current_node->str);
-		if (ret < 0)
-		{
-			builtin_error("cd: no such file or directory: ", current_node->str);
-			return (EXIT_FAILURE);
-		}
+		if (cd_dir(current_node->str) == EXIT_FAILURE)
+			return (cd_fail(oldpwd));
 	}
 	else
 	{
-		home_value = get_env("HOME", blackhole->env_list);
-		if (home_value == NULL)
-			return (EXIT_FAILURE);
-		ret = chdir(home_value);
-		if (ret < 0)
-			return (EXIT_FAILURE);
+		if (cd_home(blackhole) == EXIT_FAILURE)
+			return (cd_fail(oldpwd));
+	}
+	pwd = getcwd(NULL, 0);
+	ft_update_pwd_oldpwd(oldpwd, pwd, blackhole);
+	free(oldpwd);
+	free(pwd);
+	return (EXIT_SUCCESS);
+}
+
+static int	cd_fail(char *oldpwd)
+{
+	free(oldpwd);
+	return (EXIT_FAILURE);
+}
+
+static int	cd_dir(char *dir)
+{
+	int	ret;
+
+	ret = chdir(dir);
+	if (ret < 0)
+	{
+		builtin_error("cd: no such file or directory: ", dir);
+		return (EXIT_FAILURE);
 	}
 	return (EXIT_SUCCESS);
+}
+
+static int	cd_home(t_blackhole *blackhole)
+{
+	char	*home_dir;
+	int		ret;
+
+	home_dir = get_env("HOME", blackhole->env_list);
+	if (home_dir == NULL)
+		return (EXIT_FAILURE);
+	ret = chdir(home_dir);
+	if (ret < 0)
+		return (EXIT_FAILURE);
+	return (EXIT_SUCCESS);
+}
+
+static void	ft_update_pwd_oldpwd(char *oldpwd,
+				char *pwd, t_blackhole *blackhole)
+{
+	char	*addpwd;
+	char	*addoldpwd;
+
+	addpwd = ft_strjoin("PWD=", pwd);
+	addoldpwd = ft_strjoin("OLDPWD=", oldpwd);
+	add_env(addpwd, blackhole->env_list);
+	add_env(addoldpwd, blackhole->env_list);
+	free(addpwd);
+	free(addoldpwd);
 }
